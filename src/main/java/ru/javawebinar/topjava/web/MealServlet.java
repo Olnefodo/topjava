0be0baaf6cec521a89882c9e8a0648082baa74b4
Oleksandr <olnefodo@student.jyu.fi>
2015-12-07 11:54:50 +0200

    Prepare to HWO

---
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -0,0 +1,16 @@
+package ru.javawebinar.topjava.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Oleksandr on 07.12.15.
+ */
+public class MealServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+}
