500f75e1d972be01e006551bcb456a1c36752967
Oleksandr <olnefodo@student.jyu.fi>
2015-12-15 15:50:45 +0200

    Prepare to HW01

--- src/main/java/ru/javawebinar/topjava/model/User.java
+++ src/main/java/ru/javawebinar/topjava/model/User.java
@@ -29,6 +29,47 @@ public class User extends NamedEntity {
         this.authorities = EnumSet.of(role, roles);
     }
 
+    public String getEmail() {
+        return email;
+    }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getRegistered() {
+        return registered;
+    }
+
+    public void setRegistered(Date registered) {
+        this.registered = registered;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "User (" +
+                "name=" + name +
+                ", email=" + email +
+                ", password=" + password +
+                ", enabled=" + enabled +
+                ", registered=" + registered +
+                ')';
+    }
 
 }
