51c90598a059907d6042025a09f48fb949f8317d
Oleksandr <olnefodo@student.jyu.fi>
2015-12-16 10:40:50 +0200

    Prepare to HW01

--- src/main/java/ru/javawebinar/topjava/repository/UserMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/UserMealRepository.java
@@ -1,8 +1,16 @@
 package ru.javawebinar.topjava.repository;
 
+import ru.javawebinar.topjava.model.UserMeal;
+
+import java.util.List;
+
 /**
  * Created by Oleksandr on 15.12.15.
  */
 public interface UserMealRepository {
+
+    UserMeal save(UserMeal userMeal);
+    void delete(int id);
+    List<UserMeal> getAll();
 
 }
